c0140ddb2f68de722ac9a6b899aec35854ad62a5
kirillkonik <64696994+kirillkonik@users.noreply.github.com>
2020-08-13 16:41:52 +0300

    Update all my autotests for 13/08/2020

--- conftest.py
+++ conftest.py
@@ -19,7 +19,7 @@ from selenium import webdriver
 #fp = webdriver.FirefoxProfile()
 #fp.set_preference("intl.accept_languages", user_language)
 #browser = webdriver.Firefox(firefox_profile=fp)
-
+#sadnjandsadasd
 def pytest_addoption(parser):
     parser.addoption('--browser_name', \
                      action='store', \
